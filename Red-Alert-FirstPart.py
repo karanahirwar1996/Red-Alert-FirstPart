451180e51676bf30fcea9f3082d360b5a904ce9a
Karan Ahirwar <114184956+karanahirwar1996@users.noreply.github.com>
2023-05-14 15:05:39 +0530

    Update Red-Alert-FirstPart.py

--- Red-Alert-FirstPart.py
+++ Red-Alert-FirstPart.py
@@ -113,10 +113,11 @@ def stock_details(url):
                 password = "uccrgtqdnusrpmnk"
 
                 table_html = under2000[["ISIN",'Name', 'Price','52 Week Low','52 Week High', 'Week Return', 'New_URL']].to_html(index=False)
-                if not financial.empty:
-                    financial_html = financial.to_html(index=False)
+                if financial == None:
+                      financial_html = None
                 else:
-                    None
+                      financial_html = financial.to_html(index=False)
+
                 message = MIMEText(f"""
 <html>
   <head>
